3c4a6d0ff8eeef21a720418b2296b4065e428c3e
Fai Alateeq <37877628+fairouzAlateeq@users.noreply.github.com>
2024-09-29 20:24:13 -0500

    Update Main.java

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     static Scanner scanny = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void MortgageCalculator() {
         int principal;
         double interest, monthly_payment, months, years;
         double total_interest, monthly_rate;
@@ -19,5 +19,35 @@ public class Main {
         monthly_rate = interest/12;
         monthly_payment = principal * (monthly_rate * Math.pow(1 + monthly_rate, months)) / (Math.pow((1 +monthly_rate),months) -1 );
         System.out.println(monthly_payment);
+    }
+    public static void FutureValue(){
+        float deposit, interest, number_of_years;
+        float futureValue, totalInterest;
+        System.out.println("Enter the deposit: ");
+        deposit = scanny.nextFloat();
+        System.out.println("Enter the interest rate: ");
+        interest = scanny.nextFloat();
+        System.out.println("Enter the number of years: ");
+        number_of_years = scanny.nextFloat();
+        totalInterest = (deposit * interest) * number_of_years;
+        futureValue = deposit + totalInterest;
+        System.out.println("future value is: " + futureValue + ", And total interest rate: "+ totalInterest);
+    }
+    public static void PresentValue() {
+        float monthly_payout, interest_rate, years, present_value;
+        System.out.println("Enter the monthly_payout: ");
+        monthly_payout = scanny.nextFloat();
+        System.out.println("Enter the interest rate: ");
+        interest_rate = scanny.nextFloat();
+        System.out.println("Enter the number of years: ");
+        years = scanny.nextFloat();
+        float monthlyInterestRate = interest_rate / 12;
+        float totalPayments = years * 12;
+        //present_value = (float) (monthly_payout * (1 - Math.pow(1 + interest_rate, -totalPayments)) / monthlyInterestRate);
+        present_value = (float) (monthly_payout * (1 - Math.pow(1 + monthlyInterestRate, -totalPayments)) / monthlyInterestRate);
+        System.out.println(present_value);
+    }
+    public static void main(String[] args) {
+        PresentValue();
     }
 }
